e3220a507f3e8f706a6a7bebeec7cd7c7534980c
Reyar <2290197845@qq.com>
2025-11-11 18:22:24 +0800

    try down

--- Bot/plugins/bbdown/__init__.py
+++ Bot/plugins/bbdown/__init__.py
@@ -9,6 +9,8 @@ import time
 import logging
 import traceback
 import json
+import shutil
+import subprocess
 from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event, MessageEvent
 
 
@@ -42,14 +44,38 @@ async def bbdown_handle(event: MessageEvent):
                 data = data.replace('\\/', '/')
                 parsed = json.loads(data)
                 url = parsed['meta']['detail_1']['qqdocurl']
-                await bbdown.send(f"下载链接喵：{url}")
+
+                if "-audio" in event.get_message().extract_plain_text():
+                    await bbdown.send(f"解析成功！{url}\n选择了仅音频模式喵, 下载进程开始了喵～")
+                else :
+                    await bbdown.send(f"解析成功！{url}\n默认下载最高画质喵, 下载进程开始了喵～")
             else:
-                await bbdown.send("这不是哔哩哔哩小程序喵")
+                await bbdown.send("并非bilibili视频喵")
         else:
-            await bbdown.send("这不是哔哩哔哩小程序喵")
+            await bbdown.send("并非bilibili视频喵")
     except:
         await asyncio.sleep(0.5)
+        if "FinishedException()" in traceback.format_exc(): return
         await bbdown.send("死了喵")
         await bbdown.finish(traceback.format_exc())
 
 
+async def run_bbdown(url: str, audio_only: bool, timeout: int = 600):
+    bbdown_cmd = shutil.which("BBDown")
+    if not bbdown_cmd:
+        await bbdown.send("BBDown没找到喵")
+    proc = await asyncio.create_subprocess_exec(
+        bbdown_cmd, # type: ignore
+        url,
+        "--audio-only" if audio_only else "",
+        "--work-dir", "/root/Video")
+    try:
+        stdout, stderr = await asyncio.wait_for(proc.communicate(), timeout)
+    except asyncio.TimeoutError:
+        proc.kill()
+        await bbdown.send("下载超时喵")
+        return None
+    if proc.returncode != 0:
+        await bbdown.send("下载失败喵")
+        return None
+    return stdout
