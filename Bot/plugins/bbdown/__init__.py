a1dfde3b02f9e1eac8ba5ccb3095f98887f06b3d
Reyar <2290197845@qq.com>
2025-11-11 16:46:30 +0800

    test message event

--- Bot/plugins/bbdown/__init__.py
+++ Bot/plugins/bbdown/__init__.py
@@ -4,7 +4,8 @@ from nonebot.plugin import PluginMetadata
 from .config import Config
 from nonebot import on_command
 import requests # type: ignore
-from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event
+from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event, MessageEvent
+
 
 __plugin_meta__ = PluginMetadata(
     name="bbdown",
@@ -18,10 +19,13 @@ config = get_plugin_config(Config)
 bbdown = on_command("dl")
 
 @bbdown.handle()
-async def bbdown_handle(event: Event):
-    msgs: Message = Message()
-    for i in event.get_message():
-        msgs += i
-    await bbdown.finish(str(msgs))
+async def bbdown_handle(event: MessageEvent):
+    if not event.reply:
+        await bbdown.finish("并非reply喵")
+    reply = event.reply
+    reply_msg = reply.message
+    rich_text = reply_msg.to_rich_text()
+    await bbdown.send(reply_msg)
+    await bbdown.finish("提取到了:\n" + rich_text)
 
 
