1c7473bf3c4acee0ae94e277e2c7581ec2803ada
Reyar <2290197845@qq.com>
2025-12-22 00:56:21 +0800

    Cube Scrambler

---
+++ Bot/plugins/scramble/__init__.py
@@ -0,0 +1,34 @@
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
+from nonebot import on_command
+from .config import Config
+from nonebot.adapters.onebot.v11 import Message, MessageSegment, Event
+from .scrambler import get_scramble
+from nonebot.params import CommandArg
+import time
+
+__plugin_meta__ = PluginMetadata(
+    name="Scramble",
+    description="Generate scramble sequences for 2x2, 3x3, and 4x4 Rubik's cubes.",
+    usage="",
+    config=Config,
+)
+
+config = get_plugin_config(Config)
+
+scramble = on_command("scramble")
+
+@scramble.handle()
+async def scramble_handle(args: Message = CommandArg(), event: Event):
+    time.sleep(1)
+    id = event.get_user_id()
+    if not args.extract_plain_text():
+        await scramble.finish(MessageSegment.at(id) + " 用法：/scramble <项目>, 例如 /scramble 3")
+    elif not args.extract_plain_text() in ["2", "3", "4"]:
+        await scramble.finish(MessageSegment.at(id) + " 目前仅支持2/3/4阶魔方喵~")
+    else:
+        mode = args.extract_plain_text()
+        result = get_scramble(mode)
+        await scramble.finish(MessageSegment.at(id) + " " + result)
+    
+
---
+++ Bot/plugins/scramble/scrambler.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+def get_scramble(mode: str = "3") -> str:
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    index_path = os.path.join(BASE_DIR, "Index.json")
+    scramble_path = os.path.join(BASE_DIR, "scramble" + mode + ".txt")
+
+    with open(index_path, "r") as f:
+        data = json.load(f)
+        index = data["index"]
+
+    match mode:
+        case "3":
+            with open(scramble_path, "r", encoding="utf-8") as f:
+                scrambles = f.readlines()
+                scramble = scrambles[int(index)].strip()
+    
+    with open(index_path, "w") as f:
+        data["index"] = str((int(index) + 1) % len(scrambles))
+        json.dump(data, f)
+
+    return scramble
+
+if __name__ == "__main__":
+    print(get_scramble())
+
+
