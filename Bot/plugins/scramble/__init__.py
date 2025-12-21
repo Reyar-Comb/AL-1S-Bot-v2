64bccbd52602d346b0a2e6db8c724add469d2f25
Reyar <2290197845@qq.com>
2025-12-22 01:02:05 +0800

    fix

--- Bot/plugins/scramble/__init__.py
+++ Bot/plugins/scramble/__init__.py
@@ -19,12 +19,12 @@ config = get_plugin_config(Config)
 scramble = on_command("scramble")
 
 @scramble.handle()
-async def scramble_handle(args: Message = CommandArg(), event: Event):
+async def scramble_handle(event: Event, args: Message = CommandArg()):
     time.sleep(1)
     id = event.get_user_id()
     if not args.extract_plain_text():
         await scramble.finish(MessageSegment.at(id) + " 用法：/scramble <项目>, 例如 /scramble 3")
-    elif not args.extract_plain_text() in ["2", "3", "4"]:
+    elif args.extract_plain_text() not in ["2", "3", "4"]:
         await scramble.finish(MessageSegment.at(id) + " 目前仅支持2/3/4阶魔方喵~")
     else:
         mode = args.extract_plain_text()
